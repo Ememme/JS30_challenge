93c5de7ce50ead7ac40f8f7e8e9c6c7bc5acc226
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-12-12 16:54:11 +0100

    Feat: rewinding video and updating volume and playback rate

--- 11_Video_player/player.js
+++ 11_Video_player/player.js
@@ -3,7 +3,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const playButton = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
+const rewindButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 function togglePlay() {
@@ -18,6 +18,15 @@ function updateButtonIcon() {
     playButton.textContent = icon;
 }
 
+function rewindVideo() {
+    console.log(this.dataset.skip);
+    video.currentTime += +this.dataset.skip;
+}
+
+function updateRange() {
+    console.log(this.value);
+    video[this.name] = this.value;
+}
 
 // Hooking up event listeners:
 // Video toggles play if video window or playButton is clicked
@@ -27,3 +36,10 @@ playButton.addEventListener('click', togglePlay);
 // Updating playButton whenever video is paused/started - could be done via 3rd party plugins, etc.
 video.addEventListener('play', updateButtonIcon);
 video.addEventListener('pause', updateButtonIcon);
+
+// Rewinding video
+
+rewindButtons.forEach(button => button.addEventListener('click', rewindVideo));
+
+// Updating range values (volume and playbackRate)
+ranges.forEach(range => range.addEventListener('change', updateRange));
