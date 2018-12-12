0821fc28048acd8dabccdb6fc42802b23c460c76
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-12-12 14:49:38 +0100

    Feat: update play button icon when video is toggled

--- 11_Video_player/player.js
+++ 11_Video_player/player.js
@@ -10,9 +10,20 @@ function togglePlay() {
    video.paused ? video.play() : video.pause()
 }
 
+function updateButtonIcon() {
+    // console.log('Update me!');
+    // console.log(this);
+    const icon = this.paused ? '►' : '❚ ❚';
+    // console.log(icon);
+    playButton.textContent = icon;
+}
 
 
 // Hooking up event listeners:
 // Video toggles play if video window or playButton is clicked
 video.addEventListener('click', togglePlay);
 playButton.addEventListener('click', togglePlay);
+
+// Updating playButton whenever video is paused/started - could be done via 3rd party plugins, etc.
+video.addEventListener('play', updateButtonIcon);
+video.addEventListener('pause', updateButtonIcon);
