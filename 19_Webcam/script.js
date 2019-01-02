d4479a01381f3f9ed0c90fa4576d63f5ba9c0629
Magda Mieczynska <magda.mieczynska@gmail.com>
2019-01-02 12:25:38 +0100

    Feat: getting video input

--- 19_Webcam/script.js
+++ 19_Webcam/script.js
@@ -1,5 +1,23 @@
-const video = document.querySelector('.player');
+// import { get } from "https";
+
+// const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    // way to access the media devices
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(function (mediaStream) {
+            let video = document.querySelector('.player');
+            video.srcObject = mediaStream;
+            video.onloadedmetadata = function (e) {
+                video.play();
+            };
+        })
+        .catch(function (err) {
+            console.log(err.name + ": " + err.message);
+        });
+}
+getVideo();
