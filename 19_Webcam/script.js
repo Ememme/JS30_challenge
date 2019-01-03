21a2017864ad408474a7b327084843df13cd2e99
Magda Mieczynska <magda.mieczynska@gmail.com>
2019-01-03 11:46:59 +0100

    First attempt at printing to canvas

--- 19_Webcam/script.js
+++ 19_Webcam/script.js
@@ -1,7 +1,8 @@
 // import { get } from "https";
 
-// const video = document.querySelector('.player');
+const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
+console.log(canvas);
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
@@ -10,7 +11,7 @@ function getVideo() {
     // way to access the media devices
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then(function (mediaStream) {
-            let video = document.querySelector('.player');
+            // let video = document.querySelector('.player');
             video.srcObject = mediaStream;
             video.onloadedmetadata = function (e) {
                 video.play();
@@ -20,4 +21,21 @@ function getVideo() {
             console.log(err.name + ": " + err.message);
         });
 }
+
+
+function videoToCanvas() {
+    const width = video.videoWidth;
+    console.log(width);
+    const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+    
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16); 
+}
+
+videoToCanvas();
+
 getVideo();
