3e4b53c06d609671a2998c9be5802b8b02e3bc18
Magda Mieczynska <magda.mieczynska@gmail.com>
2019-02-14 10:48:52 +0100

    Fix to show first value of seconds

--- 29_Countdown_clock/scripts.js
+++ 29_Countdown_clock/scripts.js
@@ -12,6 +12,8 @@ function timer(seconds) {
     const endTime = startTime + seconds * 1000;
 
     console.log({startTime, endTime});
+    // displayTime used to show the initial value of seconds, as setInterval causes 1s delay in showing the value
+    displayTime(seconds);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((endTime - Date.now()) / 1000)
@@ -19,6 +21,10 @@ function timer(seconds) {
         if(secondsLeft < 0) {
             clearInterval(countdown);
         }
-        console.log(secondsLeft);
+        displayTime(secondsLeft);
     }, 1000);
+}
+
+function displayTime(seconds) {
+    console.log(seconds)
 }
