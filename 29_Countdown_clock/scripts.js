8e08f477b405f020ddc1fac111e670057ad696c5
Magda Mieczynska <magda.mieczynska@gmail.com>
2019-02-14 12:00:48 +0100

    Hook up buttons to run timer

--- 29_Countdown_clock/scripts.js
+++ 29_Countdown_clock/scripts.js
@@ -1,6 +1,8 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTimeDisplay = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+console.log(buttons);
 
 
 function timer(seconds) {
@@ -41,4 +43,12 @@ function displayEndTime(timestamp) {
     // Variable for US/Canadian repersentation of hours
     const adjustedHour = hour > 12 ? hour - 12 : hour;
     endTimeDisplay.textContent = `Be back at ${hour}:${minutes < 10? '0':''}${minutes}`
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+
+function startTimer(){
+    // Getting preset values from buttons
+    const setTime = parseInt(this.dataset.time);
+    timer(setTime);
 }
